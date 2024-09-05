f629a2eb5f796f5c5dbfaeac48b7ee6b5efe3c28
guru prasad <guruprasad8212@gmail.com>
2024-09-05 10:19:35 +0530

    redux setup

---
+++ app/App.tsx
@@ -0,0 +1,20 @@
+import { useRouter } from "expo-router";
+import { Button,Text, View } from "react-native";
+
+export const App = () =>{
+
+    const router = useRouter();
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Welcome to the Fitness App!</Text>
+      {/* Corrected Link component for navigating to the next screen */}
+
+      <Button
+        title="Get Started"
+        onPress={() => {
+          router.push("/userInput/Gender");
+        }}
+      />
+    </View>
+  );
+}
